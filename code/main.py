511b9e71d33153ec8447325cfb6d6d04d17a3eac
maxkucher <kucherenko.max75@mail.ru>
2026-02-25 16:33:09 +0400

    отладка авторизации с интранета

--- code/main.py
+++ code/main.py
@@ -161,9 +161,9 @@ async def login(
         await db.commit()
         await db.refresh(user)
 
-        redirect_url = f"http://exhibitions.kyberlox.ru/users/me"
+        # redirect_url = f"http://exhibitions.kyberlox.ru/users/me"
         #  # Создаем RedirectResponse
-        response = RedirectResponse(url=redirect_url)
+        # response = RedirectResponse(url=redirect_url)
 
         # Устанавливаем куки
         response.set_cookie(
@@ -184,17 +184,17 @@ async def login(
             max_age=30 * 24 * 60 * 60
         )
 
-        # return {
-        #     "message": "Успешная авторизация",
-        #     "user": {
-        #         "id": user.id,
-        #         "full_name": user.full_name,
-        #         "department": user.department,
-        #         "position": user.position,
-        #         "is_admin": user.is_admin
-        #     }
-        # }
-        return response
+        return {
+            "message": "Успешная авторизация",
+            "user": {
+                "id": user.id,
+                "full_name": user.full_name,
+                "department": user.department,
+                "position": user.position,
+                "is_admin": user.is_admin
+            }
+        }
+        # return response
         # if user.is_admin:
         #     return RedirectResponse(url="/exhibitions")
         # elif user.is_admin is False:
