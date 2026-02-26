920ab935ce69f235fec936d4926c3e864cbda6d7
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2026-02-26 14:07:42 +0400

    меняем редирект на новый домен org.ru

--- code/main.py
+++ code/main.py
@@ -291,7 +291,7 @@ async def login_get(
         await db.commit()
         await db.refresh(user)
 
-        redirect_url = f"http://exhibitions.emk.org.ru:8000/api/docs"
+        redirect_url = f"http://exhibitions.emk.org.ru/api/docs"
         #  # Создаем RedirectResponse
         response = RedirectResponse(url=redirect_url)
 
